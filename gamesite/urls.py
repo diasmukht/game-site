e927592536332c3c0f229ad5d9fab6d6c98ba541
diasmukht <diaspias05@gmail.com>
2025-05-20 17:35:46 +0600

    login update

--- gamesite/urls.py
+++ gamesite/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')), # Включаем URL-адреса из вашего приложения 'main'
-    path('accounts/', include('django.contrib.auth.urls')), # Встроенные URL-адреса Django для аутентификации
 ]
